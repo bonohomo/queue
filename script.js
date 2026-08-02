c33a2d6fab4130d8dac2574f0bb230f3cfa9b081
bro <anishenko.fed@yandex.ru>
2026-08-02 16:58:32 +0300

    velikiySi

--- script.js
+++ script.js
@@ -6635,6 +6635,203 @@ $ - конец строки (после последовательности н�
 // alert(localStorage.test);
 // delete localStorage.test;
 
-localStorage.setItem("username", "Марина");
-localStorage.setItem("surname", "Ветрова");
-alert(localStorage.length);
+// localStorage.setItem("username", "Марина");
+// localStorage.setItem("surname", "Ветрова");
+// // alert(localStorage.length);
+
+
+// for(let i = 0; i < localStorage.length; i++){
+//     let k = localStorage.key(i);
+//     console.log(k + ": " + localStorage.getItem(k));
+
+// }
+
+// console.log("\n");
+
+// let keys = Object.keys(localStorage); // ["username", "data", "surname"]
+// for(let key of keys){
+//     console.log(key + ": " + localStorage.getItem(key) + ", тип: " + typeof localStorage.getItem(key));
+// }
+
+
+// localStorage.user = {name: "Сергей"}
+// console.log(localStorage.user);
+
+// localStorage.user = JSON.stringify({name: "Сергей"}); // ОБЪЕКТ JS ПРЕОБРАЗОВЫВАЕМ В JSON ОБЪЕКТ (это строка)
+// let user = JSON.parse(localStorage.user) //  возвращает  json объект в js 
+// console.log(user.name);
+
+// localStorage.removeItem("username"); // удалить данные
+
+// console.log(localStorage.clear()); // очистить куки
+
+
+// КЛИКЕР С СОХРАНЕНИЕМ ДАННЫХ
+
+// let board = document.querySelector("div");
+// let value = document.querySelector("span");
+// let counter;
+
+// if(localStorage.getItem("count") > 0) {
+//     counter = localStorage.getItem("count");
+//     value.textContent = counter;
+// } else {
+//     counter = 0;
+// }
+
+// board.addEventListener('click', function(){
+//     counter++;
+//     value.textContent = counter;
+//     localStorage.setItem("count", counter);
+// });
+
+// board.addEventListener("contextmenu", function(){
+//     localStorage.clear();
+//     counter = 0;
+//     value.textContent = counter;
+// });
+
+
+
+// const form = document.getElementById("form1");
+// const formFields = form.elements;
+// const submitBtn = form.querySelector("[type='submit']");
+
+// for (let i = 0; i < formFields.length; i++) {
+//     formFields[i].addEventListener('change', changeHandle);
+// }
+
+// function changeHandle() {
+//     if(this.type != "checkbox") {
+//         console.log(this.name, this.value);
+//         localStorage.setItem(this.name, this.value);
+//     } else {
+//         console.log(this.name, this.checked);
+//         localStorage.setItem(this.name, this. checked);
+//     }
+// }
+
+// checkedStorage();
+
+// function checkedStorage() {
+//     for (let i = 0; i < formFields.length; i++) {
+//         if(formFields[i].type === "submit"){
+//             if(formFields[i].type === "checkbox"){
+//               formFields[i].checked = localStorage.getItem(formFields[i].name);  
+//             } else {
+//                 formFields[i].value = localStorage.getItem(formFields[i].name);
+//             }
+//         }
+//     }
+
+//     attachEvents();
+// }
+
+// // допис
+
+
+
+// let list = document.querySelector("#list");
+// let but = document.querySelector("button");
+
+// but.addEventListener("click", addNote);
+
+// function addNote() {
+//     let name = prompt("Имя записи", "");
+
+//     notes[name] = "";
+//     addToList(name);
+//     saveToStorage();
+//     list.value = name;
+//     current.value = notes[name];
+// }
+
+// function addToList(name) {
+//     let option = document.createElement("option");
+//     option.textContent = name;
+//     list.append(option);
+// }
+
+// function saveToStorage() {
+//     localStorage.setItem("notes", JSON.stringify(notes));
+// }
+
+// let notes = JSON.parse(localStorage.getItem("notes")) || {"что купить": ""};
+
+// for(let name in notes) {
+//     addToList(name);
+// }
+
+// let current = document.querySelector("#currentnote");
+// current.value = notes[list.value];
+
+// list.addEventListener("change", function() {
+//     current.value = notes[list.value];
+// });
+
+// current.addEventListener("change", function(){
+//     notes[list.value] = current.value;
+//     saveToStorage();
+// });
+
+
+// sessionStorage.setItem("test", 1);
+// console.log(sessionStorage.getItem("test"));
+
+
+// sessionStorage.setItem(
+//     "user",
+//     JSON.stringify({
+//         name: "Igor",
+//         age: 23,
+//         isDeveloper: true
+//     })
+// )
+// console.log(sessionStorage.getItem("user")); // получаем в виде строки
+// console.log("user:", JSON.parse(sessionStorage.getItem("user"))); // так получим данные нормально
+
+// sessionStorage.removeItem("test");
+
+// sessionStorage.clear();
+
+
+class ThemeSwitcher {
+    constructor() {
+        this.selectors = {switcherThemeButton: '[data-switcher]'};
+        this.themes = {
+            dark: 'dark',
+            light: 'light'
+        }
+
+        this.stateClasses = {isDarkTheme: "is-dark-theme"};
+
+        this.storageKey = "theme";
+
+        this.switcherThemeButtonElement = document.querySelector(this.selectors.switcherThemeButton);
+
+        this.setInitialTheme()
+    }
+
+    get isDarkThemeCached() {
+        return sessionStorage.getItem(this.storageKey) === this.themes.dark;
+    }
+
+    setInitialTheme() {
+        document.documentElement.classList.toggle(this.stateClasses.isDarkTheme, this.darkThemeCached);
+    }
+
+    onClick = () => {
+        sessionStorage.setItem(
+            this.storageKey,
+            this.isDarkThemeCached ? this.themes.light : this.themes.dark
+        )
+
+        document.documentElement.classList.toggle(this.stateClasses.isDarkTheme);
+    }
+
+    bindEvents() {
+        this.switcherThemeButtonElement.addEventListener("click", this.onClick);
+    }
+}
+
+new ThemeSwitcher();
